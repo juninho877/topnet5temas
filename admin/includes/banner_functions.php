b2beb08b2797256fca80f0b4281ed0090fa2d8a7
juninho541 <ffomito541@gmail.com>
2025-07-02 18:43:50 -0300

    5

--- admin/includes/banner_functions.php
+++ admin/includes/banner_functions.php
@@ -1278,7 +1278,7 @@ function _gerarBannerModel5($userId, $jogos, $grupoJogos, $width, $height, $padd
 
     
     if ($logoL) {
-        imagecopy($im, $logoL, 40, 1500, 0, 0, imagesx($logoL), imagesy($logoL));
+        imagecopy($im, $logoL, 0, 1550, 0, 0, 1080, imagesy($logoL));
     }
     // Cabeçalho com o novo layout
     $fonteTitulo = __DIR__ . '/../fonts/BebasNeue-Regular.ttf';
